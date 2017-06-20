208e465abdeb9479695642571eb3d3368167edd4
Matt Insler <mattinsler@users.noreply.github.com>
2017-06-20 02:13:54 -0700

    improved type conversions and typings

--- lib/bittrex-client.d.ts
+++ lib/bittrex-client.d.ts
@@ -8,6 +8,7 @@ export declare namespace Bittrex {
         Pending: Decimal;
         CryptoAddress: string | null;
     }
+    const BalanceConverter: (obj: any) => Balance;
     interface Currency {
         Currency: string;
         CurrencyLong: string;
@@ -17,6 +18,11 @@ export declare namespace Bittrex {
         CoinType: string;
         BaseAddress: string | null;
     }
+    const CurrencyConverter: (obj: any) => Currency;
+    interface DepositAddress {
+        Currency: string;
+        Address: string;
+    }
     interface Market {
         MarketCurrency: string;
         BaseCurrency: string;
@@ -25,17 +31,19 @@ export declare namespace Bittrex {
         MinTradeSize: Decimal;
         MarketName: string;
         IsActive: boolean;
-        Created: string;
+        Created: Date;
     }
+    const MarketConverter: (obj: any) => Market;
     interface MarketHistory {
         Id: number;
-        TimeStamp: string;
+        TimeStamp: Date;
         Quantity: Decimal;
         Price: Decimal;
         Total: Decimal;
         FillType: 'FILL' | 'PARTIAL_FILL';
         OrderType: 'BUY' | 'SELL';
     }
+    const MarketHistoryConverter: (obj: any) => MarketHistory;
     interface MarketSummary {
         MarketName: string;
         High: Decimal;
@@ -43,19 +51,64 @@ export declare namespace Bittrex {
         Volume: Decimal;
         Last: Decimal;
         BaseVolume: Decimal;
-        TimeStamp: string;
+        TimeStamp: Date;
         Bid: Decimal;
         Ask: Decimal;
         OpenBuyOrders: number;
         OpenSellOrders: number;
         PrevDay: Decimal;
-        Created: string;
+        Created: Date;
         DisplayMarketName: string | null;
     }
+    const MarketSummaryConverter: (obj: any) => MarketSummary;
+    interface Order {
+        AccountId: string | null;
+        OrderUuid: string;
+        Exchange: string;
+        Type: string;
+        Quantity: Decimal;
+        QuantityRemaining: Decimal;
+        Limit: Decimal;
+        Reserved: Decimal;
+        ReserveRemaining: Decimal;
+        CommissionReserved: Decimal;
+        CommissionReserveRemaining: Decimal;
+        CommissionPaid: Decimal;
+        Price: Decimal;
+        PricePerUnit: Decimal | null;
+        Opened: Date;
+        Closed: null;
+        IsOpen: boolean;
+        Sentinel: string;
+        CancelInitiated: boolean;
+        ImmediateOrCancel: boolean;
+        IsConditional: boolean;
+        Condition: string;
+        ConditionTarget: null;
+    }
+    const OrderConverter: (obj: any) => Order;
+    interface OrderHistoryOrder {
+        OrderUuid: string;
+        Exchange: string;
+        TimeStamp: Date;
+        OrderType: string;
+        Limit: Decimal;
+        Quantity: Decimal;
+        QuantityRemaining: Decimal;
+        Commission: Decimal;
+        Price: Decimal;
+        PricePerUnit: null;
+        IsConditional: boolean;
+        Condition: null;
+        ConditionTarget: null;
+        ImmediateOrCancel: boolean;
+    }
+    const OrderHistoryOrderConverter: (obj: any) => OrderHistoryOrder;
     interface OrderBookOrder {
         Quantity: Decimal;
         Rate: Decimal;
     }
+    const OrderBookOrderConverter: (obj: any) => OrderBookOrder;
     interface OrderBook {
         buy: OrderBookOrder[];
         sell: OrderBookOrder[];
@@ -64,6 +117,24 @@ export declare namespace Bittrex {
         Bid: Decimal;
         Ask: Decimal;
         Last: Decimal;
+    }
+    const TickerConverter: (obj: any) => Ticker;
+    interface Transaction {
+        PaymentUuid: string;
+        Currency: string;
+        Amount: Decimal;
+        Address: string;
+        Opened: Date;
+        Authorized: boolean;
+        PendingPayment: boolean;
+        TxCost: Decimal;
+        TxId: string | null;
+        Canceled: boolean;
+        InvalidAddress: boolean;
+    }
+    const TransactionConverter: (obj: any) => Transaction;
+    interface WithdrawalConfirmation {
+        uuid: string;
     }
     interface Client {
         markets(): Promise<Market[]>;
@@ -74,6 +145,13 @@ export declare namespace Bittrex {
         orderBook(market: string, type: 'buy' | 'sell' | 'both', depth?: number): Promise<OrderBook>;
         marketHistory(market: string): Promise<MarketHistory[]>;
         balances(): Promise<Balance[]>;
+        balance(currency: string): Promise<Balance>;
+        depositAddress(currency: string): Promise<DepositAddress>;
+        withdraw(currency: string, quantity: Decimal, address: string, paymentid?: string): Promise<WithdrawalConfirmation>;
+        order(uuid: string): Promise<Order>;
+        orderHistory(market?: string): Promise<OrderHistoryOrder[]>;
+        withdrawalHistory(currency?: string): Promise<Transaction[]>;
+        depositHistory(currency?: string): Promise<Transaction[]>;
     }
 }
 export interface Options {
@@ -95,4 +173,11 @@ export declare class Client implements Bittrex.Client {
     orderBook(market: string, type?: 'buy' | 'sell' | 'both', depth?: number): Promise<Bittrex.OrderBook>;
     marketHistory(market: string): Promise<Bittrex.MarketHistory[]>;
     balances(): Promise<Bittrex.Balance[]>;
+    balance(currency: string): Promise<Bittrex.Balance>;
+    depositAddress(currency: string): Promise<Bittrex.DepositAddress>;
+    withdraw(currency: string, quantity: Bittrex.Decimal, address: string, paymentid?: string): Promise<Bittrex.WithdrawalConfirmation>;
+    order(uuid: string): Promise<Bittrex.Order>;
+    orderHistory(market?: string): Promise<Bittrex.OrderHistoryOrder[]>;
+    withdrawalHistory(currency?: string): Promise<Bittrex.Transaction[]>;
+    depositHistory(currency?: string): Promise<Bittrex.Transaction[]>;
 }
--- src/bittrex-client.ts
+++ src/bittrex-client.ts
@@ -6,29 +6,33 @@ const got = require('got');
 
 Decimal.config({ precision: 8 });
 
-type ObjectType = {[k:string]: any};
+function removeUndefined(obj: any) {
+  const res = Object.assign({}, obj);
 
-function createFieldConverter<T>(converter: (v: any) => T, ...fields: string[]) {
-  const fieldSet = new Set(fields);
+  for (const k of Object.keys(obj)) {
+    if (obj[k] === undefined) {
+      delete obj[k];
+    }
+  }
 
-  return function(obj: ObjectType): ObjectType {
-    return Object.keys(obj).reduce((res: ObjectType, key: string) => {
-      if (fieldSet.has(key)) {
-        res[key] = converter(obj[key]);
-      } else {
-        res[key] = obj[key];
-      }
-      return res;
-    }, {});
-  };
+  return res;
 }
 
-function removeUndefined(obj: ObjectType): ObjectType {
-  return Object.keys(obj).reduce((o: ObjectType, k) => {
-    const v = obj[k];
-    if (v !== undefined) { o[k] = v; }
-    return o;
-  }, {});
+const DateConverter = (v: string) => new Date(v);
+const DecimalConverter = (v: number | string) => new Decimal(v);
+
+function buildConverter<T>(config: { [k:string]: (v: any) => any }) {
+  return function(obj: any): T {
+    const res = Object.assign({}, obj);
+
+    for (const k of Object.keys(config)) {
+      if (res[k] != null) {
+        res[k] = config[k](res[k]);
+      }
+    }
+
+    return res;
+  }
 }
 
 export namespace Bittrex {
@@ -44,6 +48,12 @@ export namespace Bittrex {
     // Uuid: string | null;
   };
 
+  export const BalanceConverter = buildConverter<Balance>({
+    Balance: DecimalConverter,
+    Available: DecimalConverter,
+    Pending: DecimalConverter
+  })
+
   export interface Currency {
     Currency: string; // BTC
     CurrencyLong: string; // Bitcoin
@@ -54,6 +64,15 @@ export namespace Bittrex {
     BaseAddress: string | null;
   };
 
+  export const CurrencyConverter = buildConverter<Currency>({
+    TxFee: DecimalConverter
+  });
+
+  export interface DepositAddress {
+    Currency: string; // VTC
+		Address: string; // Vy5SKeKGXUHKS2WVpJ76HYuKAu3URastUo
+  };
+
   export interface Market {
     MarketCurrency: string; // LTC
     BaseCurrency: string; // BTC
@@ -62,18 +81,30 @@ export namespace Bittrex {
     MinTradeSize: Decimal;
     MarketName: string; // BTC-LTC
     IsActive: boolean;
-    Created: string; // 2014-02-13T00:00:00
+    Created: Date; // 2014-02-13T00:00:00
   };
+
+  export const MarketConverter = buildConverter<Market>({
+    MinTradeSize: DecimalConverter,
+    Created: DateConverter
+  });
 
   export interface MarketHistory {
     Id: number;
-    TimeStamp: string; // 2014-07-09T03:21:20.08
+    TimeStamp: Date; // 2014-07-09T03:21:20.08
     Quantity: Decimal;
     Price: Decimal;
     Total: Decimal;
     FillType: 'FILL' | 'PARTIAL_FILL';
     OrderType: 'BUY' | 'SELL';
   };
+
+  export const MarketHistoryConverter = buildConverter<MarketHistory>({
+    TimeStamp: DateConverter,
+    Quantity: DecimalConverter,
+    Price: DecimalConverter,
+    Total: DecimalConverter
+  });
 
   export interface MarketSummary {
     MarketName: string;
@@ -82,31 +113,145 @@ export namespace Bittrex {
     Volume: Decimal;
     Last: Decimal;
     BaseVolume: Decimal;
-    TimeStamp: string; // 2014-07-09T07:19:30.15
+    TimeStamp: Date; // 2014-07-09T07:19:30.15
     Bid: Decimal;
     Ask: Decimal;
     OpenBuyOrders: number;
     OpenSellOrders: number;
     PrevDay: Decimal;
-    Created: string; // 2014-03-20T06:00:00
+    Created: Date; // 2014-03-20T06:00:00
     DisplayMarketName: string | null;
   };
+
+  export const MarketSummaryConverter = buildConverter<MarketSummary>({
+    High: DecimalConverter,
+    Low: DecimalConverter,
+    Volume: DecimalConverter,
+    Last: DecimalConverter,
+    BaseVolume: DecimalConverter,
+    TimeStamp: DateConverter,
+    Bid: DecimalConverter,
+    Ask: DecimalConverter,
+    PrevDay: DecimalConverter,
+    Created: DateConverter
+  });
+
+  export interface Order {
+    AccountId: string | null; // null
+		OrderUuid: string; // 0cb4c4e4-bdc7-4e13-8c13-430e587d2cc1
+		Exchange: string; // BTC-SHLD
+		Type: string; // LIMIT_BUY
+		Quantity: Decimal; // 1000.00000000
+		QuantityRemaining: Decimal; // 1000.00000000
+		Limit: Decimal; // 0.00000001
+		Reserved: Decimal; // 0.00001000
+		ReserveRemaining: Decimal; // 0.00001000
+		CommissionReserved: Decimal; // 0.00000002
+		CommissionReserveRemaining: Decimal; // 0.00000002
+		CommissionPaid: Decimal; // 0.00000000
+		Price: Decimal; // 0.00000000
+		PricePerUnit: Decimal | null; // null
+		Opened: Date; // 2014-07-13T07:45:46.27
+		Closed: null; // null
+		IsOpen: boolean; // true
+		Sentinel: string; // 6c454604-22e2-4fb4-892e-179eede20972
+		CancelInitiated: boolean; // false
+		ImmediateOrCancel: boolean; // false
+		IsConditional: boolean; // false
+		Condition: string; // NONE
+		ConditionTarget: null; // null
+  };
+
+  export const OrderConverter = buildConverter<Order>({
+    Quantity: DecimalConverter,
+		QuantityRemaining: DecimalConverter,
+		Limit: DecimalConverter,
+		Reserved: DecimalConverter,
+		ReserveRemaining: DecimalConverter,
+		CommissionReserved: DecimalConverter,
+		CommissionReserveRemaining: DecimalConverter,
+		CommissionPaid: DecimalConverter,
+		Price: DecimalConverter,
+		PricePerUnit: DecimalConverter,
+		Opened: DateConverter
+  });
+
+  export interface OrderHistoryOrder {
+    OrderUuid: string; // fd97d393-e9b9-4dd1-9dbf-f288fc72a185
+    Exchange: string; // BTC-LTC
+    TimeStamp: Date; // 2014-07-09T04:01:00.667
+    OrderType: string; // LIMIT_BUY
+    Limit: Decimal; // 0.00000001
+    Quantity: Decimal; // 100000.00000000
+    QuantityRemaining: Decimal; // 100000.00000000
+    Commission: Decimal; // 0.00000000
+    Price: Decimal; // 0.00000000
+    PricePerUnit: null; // null
+    IsConditional: boolean; // false
+    Condition: null; // null
+    ConditionTarget: null; // null
+    ImmediateOrCancel: boolean; // false
+  };
+
+  export const OrderHistoryOrderConverter = buildConverter<OrderHistoryOrder>({
+    TimeStamp: DateConverter,
+    Limit: DecimalConverter,
+    Quantity: DecimalConverter,
+    QuantityRemaining: DecimalConverter,
+    Commission: DecimalConverter,
+    Price: DecimalConverter
+  });
 
   export interface OrderBookOrder {
     Quantity: Decimal;
     Rate: Decimal;
   };
 
+  export const OrderBookOrderConverter = buildConverter<OrderBookOrder>({
+    Quantity: DecimalConverter,
+    Rate: DecimalConverter
+  });
+
   export interface OrderBook {
     buy: OrderBookOrder[];
     sell: OrderBookOrder[];
-  }
+  };
 
   export interface Ticker {
     Bid: Decimal;
 		Ask: Decimal;
 		Last: Decimal;
-  }
+  };
+
+  export const TickerConverter = buildConverter<Ticker>({
+    Bid: DecimalConverter,
+		Ask: DecimalConverter,
+		Last: DecimalConverter
+  });
+
+  export interface Transaction {
+    PaymentUuid: string; // b52c7a5c-90c6-4c6e-835c-e16df12708b1
+    Currency: string; // BTC
+    Amount: Decimal; // 17.00000000
+    Address: string; // 1DeaaFBdbB5nrHj87x3NHS4onvw1GPNyAu
+    Opened: Date; // 2014-07-09T04:24:47.217
+    Authorized: boolean; // true
+    PendingPayment: boolean; // false
+    TxCost: Decimal; // 0.00020000
+    TxId: string | null; // 3efd41b3a051433a888eed3ecc174c1d025a5e2b486eb418eaaec5efddda22de | null
+    Canceled: boolean; // true
+    InvalidAddress: boolean; // false
+  };
+
+  export const TransactionConverter = buildConverter<Transaction>({
+    Amount: DecimalConverter,
+    Opened: DateConverter,
+    TxCost: DecimalConverter
+  });
+
+  export interface WithdrawalConfirmation {
+    uuid: string; // 68b5a16c-92de-11e3-ba3b-425861b86ab6
+  };
 
   export interface Client {
     // public
@@ -123,6 +268,13 @@ export namespace Bittrex {
 
     // account
     balances(): Promise<Balance[]>;
+    balance(currency: string): Promise<Balance>;
+    depositAddress(currency: string): Promise<DepositAddress>;
+    withdraw(currency: string, quantity: Decimal, address: string, paymentid?: string): Promise<WithdrawalConfirmation>;
+    order(uuid: string): Promise<Order>;
+    orderHistory(market?: string): Promise<OrderHistoryOrder[]>;
+    withdrawalHistory(currency?: string): Promise<Transaction[]>;
+    depositHistory(currency?: string): Promise<Transaction[]>;
   }
 };
 
@@ -193,70 +345,66 @@ export class Client implements Bittrex.Client {
   }
 
   public async markets(): Promise<Bittrex.Market[]> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'MinTradeSize'
-    );
-    return (await this.request('/public/getmarkets')).map(converter);
+    return (await this.request('/public/getmarkets')).map(Bittrex.MarketConverter);
   }
 
   public async currencies(): Promise<Bittrex.Currency[]> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'TxFee'
-    );
-    return (await this.request('/public/getcurrencies')).map(converter);
+    return (await this.request('/public/getcurrencies')).map(Bittrex.CurrencyConverter);
   }
 
   public async ticker(market: string): Promise<Bittrex.Ticker> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'Bid', 'Ask', 'Last'
-    );
-    return converter(await this.request('/public/getticker', { market })) as Bittrex.Ticker;
+    return Bittrex.TickerConverter(await this.request('/public/getticker', { market }));
   }
 
   public async marketSummaries(): Promise<Bittrex.MarketSummary[]> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'High', 'Low', 'Volume', 'Last', 'BaseVolume', 'Bid', 'Ask', 'PrevDay'
-    );
-    return (await this.request('/public/getmarketsummaries')).map(converter);
+    return (await this.request('/public/getmarketsummaries')).map(Bittrex.MarketSummaryConverter);
   }
 
   public async marketSummary(market: string): Promise<Bittrex.MarketSummary> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'High', 'Low', 'Volume', 'Last', 'BaseVolume', 'Bid', 'Ask', 'PrevDay'
-    );
-    return (await this.request('/public/getmarketsummary', { market })).map(converter)[0];
+    return (await this.request('/public/getmarketsummary', { market })).map(Bittrex.MarketSummaryConverter)[0];
   }
 
   public async orderBook(market: string, type: 'buy' | 'sell' | 'both' = 'both', depth?: number): Promise<Bittrex.OrderBook> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'Quantity', 'Rate'
-    );
     const res = await this.request('/public/getorderbook', { market, type, depth });
     return {
-      buy: res.buy.map(converter),
-      sell: res.sell.map(converter)
+      buy: res.buy.map(Bittrex.OrderBookOrderConverter),
+      sell: res.sell.map(Bittrex.OrderBookOrderConverter)
     }
   }
 
   public async marketHistory(market: string): Promise<Bittrex.MarketHistory[]> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'Quantity', 'Price', 'Total'
-    );
-    return (await this.request('/account/getmarkethistory', { market })).map(converter);
+    return (await this.request('/account/getmarkethistory', { market })).map(Bittrex.MarketHistoryConverter);
   }
 
   public async balances(): Promise<Bittrex.Balance[]> {
-    const converter = createFieldConverter(
-      v => new Decimal(v),
-      'Balance', 'Available', 'Pending'
-    );
-    return (await this.request('/account/getbalances')).map(converter);
-  }  
+    return (await this.request('/account/getbalances')).map(Bittrex.BalanceConverter);
+  }
+
+  public async balance(currency: string): Promise<Bittrex.Balance> {
+    return Bittrex.BalanceConverter(await this.request('/account/getbalance', { currency }));
+  }
+
+  public depositAddress(currency: string): Promise<Bittrex.DepositAddress> {
+    return this.request('/account/getdepositaddress', { currency });
+  }
+
+  public async withdraw(currency: string, quantity: Bittrex.Decimal, address: string, paymentid?: string): Promise<Bittrex.WithdrawalConfirmation> {
+    return await this.request('/account/withdraw', { currency, quantity: quantity.toNumber(), address, paymentid });
+  }
+
+  public async order(uuid: string): Promise<Bittrex.Order> {
+    return Bittrex.OrderConverter(await this.request('/account/getorder', { uuid }));
+  }
+
+  public async orderHistory(market?: string): Promise<Bittrex.OrderHistoryOrder[]> {
+    return (await this.request('/account/getorderhistory', { market })).map(Bittrex.OrderHistoryOrderConverter);
+  }
+
+  public async withdrawalHistory(currency?: string): Promise<Bittrex.Transaction[]> {
+    return (await this.request('/account/getwithdrawalhistory', { currency })).map(Bittrex.TransactionConverter);
+  }
+
+  public async depositHistory(currency?: string): Promise<Bittrex.Transaction[]> {
+    return (await this.request('/account/getdeposithistory', { currency })).map(Bittrex.TransactionConverter);
+  }
 }
